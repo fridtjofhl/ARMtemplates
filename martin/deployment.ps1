3ebb213a5973ace81833e8e1bb80a59e56e03625
JENSEN Martin <martin.jensen@soprasteria.com>
2016-06-22 15:58:13 +0200

    Adds a simple deployment script

---
+++ martin/deployment.ps1
@@ -0,0 +1,13 @@
+﻿$resourceGroup = 'R01'
+$deployment = 'R01D'
+$subscriptionName = 'Free Trial'
+$location = 'North Europe'
+$templateFile = 'template.json'
+$parameterFile = 'parameters.json'
+
+Add-AzureRmAccount
+Set-AzureRmContext -SubscriptionName $subscriptionName
+
+New-AzureRmResourceGroup -Name $resourceGroup -Location $location
+
+New-AzureRmResourceGroupDeployment -Name $deployment -ResourceGroupName $resourceGroup -TemplateFile $templateFile -TemplateParameterFile $parameterFile
