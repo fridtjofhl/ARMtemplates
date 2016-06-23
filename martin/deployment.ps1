0b1ee277ddd40c3a8165e10c52aa5b9b07fc6ba3
Martin Jensen <martin.jensen@soprasteria.com>
2016-06-23 16:28:41 +0200

    Finishes milestone 1

--- martin/deployment.ps1
+++ martin/deployment.ps1
@@ -1,13 +1,13 @@
-﻿$resourceGroup = 'R01'
-$deployment = 'R01D'
+﻿$resourceGroup = 'deployment-test2'
+$deployment = 'D1'
 $subscriptionName = 'Free Trial'
 $location = 'North Europe'
-$templateFile = 'template.json'
-$parameterFile = 'parameters.json'
+$templateFile = 'milestone1-template.json'
+$parameterFile = 'milestone1-parameters.json'
 
 Add-AzureRmAccount
 Set-AzureRmContext -SubscriptionName $subscriptionName
 
 New-AzureRmResourceGroup -Name $resourceGroup -Location $location
 
-New-AzureRmResourceGroupDeployment -Name $deployment -ResourceGroupName $resourceGroup -TemplateFile $templateFile -TemplateParameterFile $parameterFile
+New-AzureRmResourceGroupDeployment -Mode Complete -Name $deployment -ResourceGroupName $resourceGroup -TemplateFile $templateFile -TemplateParameterFile $parameterFile -Verbose
