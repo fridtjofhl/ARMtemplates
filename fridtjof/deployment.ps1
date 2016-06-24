0ad89040230d9ab0e32143df19732729bd35c8cc
Fridtjof Hegna Letting <Fridtjof.letting@soprasteria.com>
2016-06-24 13:06:18 +0200

    Milestone 2 tamplates ok, mangler domene

--- fridtjof/deployment.ps1
+++ fridtjof/deployment.ps1
@@ -1,13 +1,21 @@
-﻿$resourceGroup = 'R07'
-$deployment = 'R07D'
+﻿#OPPRETTE RESOURCEGROUP OG LEGGE TIL RESSURS
+
+#Parametere
+$resourceGroup = 'AzureDomene'
+$deployment = 'Win2012'
 $subscriptionName = 'Free Trial'
 $location = 'North Europe'
 $templateFile = 'template.json'
 $parameterFile = 'parameters.json'
 
+#Logger inn på Azurekonto
 Add-AzureRmAccount
+#Setter subscriptionID til miljøet
 Set-AzureRmContext -SubscriptionName $subscriptionName
-
+#Setter navn på ResourceGroup og lokasjon
 New-AzureRmResourceGroup -Name $resourceGroup -Location $location
+#Deploye en lokal Json tamplate
+New-AzureRmResourceGroupDeployment -Name $deployment -ResourceGroupName $resourceGroup -TemplateFile $templateFile -TemplateParameterFile $parameterFile -Verbose
 
-New-AzureRmResourceGroupDeployment -Name $deployment -ResourceGroupName $resourceGroup -TemplateFile $templateFile -TemplateParameterFile $parameterFile
+#Deploye en ekstern Json template fra URL
+#New-AzureRmResourceGroupDeployment -Name $DeploymentName -ResourceGroupName $ResourceGroupName -TemplateUri $PathToTemplate
