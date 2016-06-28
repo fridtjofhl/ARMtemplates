cefc4ffb3c0b1c57b11f8582dddda1cfeb231fde
Martin Jensen <martin.jensen@soprasteria.com>
2016-06-28 09:21:39 +0200

    Milestone 3-ish

--- martin/deployment.ps1
+++ martin/deployment.ps1
@@ -1,4 +1,4 @@
-﻿$resourceGroup = 'Milestone2-5'
+﻿$resourceGroup = 'Milestone2-9'
 $deployment = 'D1'
 $subscriptionName = 'Free Trial'
 $location = 'North Europe'
@@ -10,7 +10,7 @@ Add-AzureRmAccount
 Set-AzureRmContext -SubscriptionName $subscriptionName
 
 #Create a new resource group. If the resource group to deploy to already exists, this step can be skipped.
-#Note that if the resource group to be created already exists, this step is ignored.
+#Note that if the resource group to be created already exists, the command prompts to update the resource group.
 New-AzureRmResourceGroup -Name $resourceGroup -Location $location
 
 #Deploy the ARM template.
