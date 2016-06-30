b55e2c618bf882280586473b61ef138c81efb3f7
Martin Jensen <martin.jensen@soprasteria.com>
2016-06-30 12:34:00 +0200

    separates deployment script into local and external templates

---
+++ martin/deployment-external.ps1
@@ -0,0 +1,32 @@
+﻿#The resource group to deploy to
+$resourceGroup = 'mile3'
+
+#The name of the deployment
+$deployment = 'D1'
+
+# The name of the subscription used with this deployment
+$subscriptionName = 'Free Trial'
+
+# The location of the resource group
+$location = 'North Europe'
+
+# The link to the Uri containing the template
+$templateUri = 'https://raw.githubusercontent.com/fridtjofhl/ARMtemplates/master/martin/milestone1.json'
+
+# The link to the Uri containing the template parameters
+$parameterUri = 'https://raw.githubusercontent.com/fridtjofhl/ARMtemplates/master/martin/milestone1-parameters.json'
+
+# The deployment mode. Can be either Complete or Incremental
+$mode = 'Complete'
+
+# Log in to account and specify the subscription type. 
+# This is necessary because a single account may have several subscriptions
+Add-AzureRmAccount
+Set-AzureRmContext -SubscriptionName $subscriptionName
+
+# Create a new resource group.
+# If the resource group to be created already exists, the command prompts to update the resource group.
+New-AzureRmResourceGroup -Name $resourceGroup -Location $location
+
+# Deploy the ARM template.
+New-AzureRmResourceGroupDeployment -Mode $mode -Name $deployment -ResourceGroupName $resourceGroup -TemplateUri $templateUri -TemplateParameterUri $parameterUri -Verbose
---
+++ martin/deployment-local.ps1
@@ -0,0 +1,34 @@
+﻿#The resource group to deploy to
+$resourceGroup = 'mile3'
+
+#The name of the deployment
+$deployment = 'D1'
+
+# The name of the subscription used with this deployment
+$subscriptionName = 'Free Trial'
+
+# The location of the resource group
+$location = 'North Europe'
+
+# The path to the file containing the template
+$templateFile = 'milestone1.json'
+
+# The path to the file containing the template parameters
+$parameterFile = 'milestone1-parameters.json'
+
+# The deployment mode. Can be either Complete or Incremental
+$mode = 'Complete'
+
+# Log in to account and specify the subscription type. 
+# This is necessary because a single account may have several subscriptions
+Add-AzureRmAccount
+Set-AzureRmContext -SubscriptionName $subscriptionName
+
+# Create a new resource group.
+# If the resource group to be created already exists, the command prompts to update the resource group.
+New-AzureRmResourceGroup -Name $resourceGroup -Location $location
+
+# Deploy the ARM template.
+New-AzureRmResourceGroupDeployment -Mode $mode -Name $deployment -ResourceGroupName $resourceGroup -TemplateFile $templateFile -TemplateParameterFile $parameterFile -Verbose
+
+
